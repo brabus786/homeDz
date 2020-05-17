f12a6ac8e7e7b10e82ce22f09b034f6f59ba969f
unknown <nikolay.pylypchatin@gmail.com>
2020-05-17 15:38:59 +0300

    next2

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,21 +7,10 @@ import Tasks from '../tascs/tasks';
 const App = () => {
 
   const todoData = [
-<<<<<<< HEAD
     {label:'drink coffee',important:false,id:0},
     {label:'learn react.js',important:false,id:1},
     {label:'i slip',important:false,id:2},
   ]
-=======
-    {label:'drink coffee',important:false,id:1},
-    {label:'learn react.js',important:true,id:2},
-    {label:'i slip',important:false,id:3},
-  ];
-
- 
-
-
->>>>>>> 14b24b0fbfe07f165ca4e04a0adaa1fb66de6a52
 
   const [state,setState] = useState(todoData);
 
--- src/components/to-do-list/To-do-list.jsx
+++ src/components/to-do-list/To-do-list.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import ToDoListItem from '../to-do-list-item/To-do-list-item';
 import './ToDoList.css';
@@ -12,31 +11,12 @@ const ToDoList = ({ todos, deleteItem, important }) => {
        
         return (
             <li  key={id}>
-=======
-import React,{useState} from 'react';
-import ToDoListItem from '../to-do-list-item/To-do-list-item';
-import './ToDoList.css';
-
-const ToDoList = ({ todos }) => {
-    const onClickImportant = () => {
-        console.dir('sd');
-      }
-    const elements = todos.map((item) => {
-        const { id, ...itemProps } = item;    
-        return (
-            <li className='list-group-item' key={id}>
->>>>>>> 14b24b0fbfe07f165ca4e04a0adaa1fb66de6a52
                 <ToDoListItem
                     {...itemProps}
                 />
                 <div className='wrap'>
-<<<<<<< HEAD
                     <i onClick={() => deleteItem(id)} className="fas fa-trash-alt delete"></i>
                     <i onClick={() => important(id)} className="fas fa-exclamation exclamation"></i>
-=======
-                    <i className="fas fa-trash-alt delete"></i>
-                    <i onClick={onClickImportant} className="fas fa-exclamation exclamation"></i>
->>>>>>> 14b24b0fbfe07f165ca4e04a0adaa1fb66de6a52
                 </div>
             </li>
 
